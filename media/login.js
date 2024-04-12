873d4650fa1061dd52725b91f8649b434d4d2ea9
Serotonin-pepperonin <104297454+Serotonin-pepperonin@users.noreply.github.com>
2024-04-12 17:47:58 +0500

    fix

--- media/login.js
+++ media/login.js
@@ -1,35 +1,44 @@
-(function () {
-  const vscode = acquireVsCodeApi();
+const vscode = acquireVsCodeApi();
 
-  console.log("start login");
-  document
-    .getElementById("loginForm")
-    .addEventListener("submit", async (event) => {
-      event.preventDefault();
-      const email = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
+const button = document.getElementById("log");
+button.addEventListener("click", () => {
+  console.log("something");
+  vscode.postMessage({
+    command: "onError",
+    value: "error",
+  });
+});
+button.innerHTML = "text from js";
 
-      try {
-        const response = await fetch(
-          "https://app.purpleschool.ru/api-v2/auth/login",
-          {
-            method: "POST",
-            body: JSON.stringify({ email, password }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        const data = await response.json();
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-        // console.log(data);
+    try {
+      const response = await fetch(
+        "https://app.purpleschool.ru/api-v2/auth/login",
+        {
+          method: "POST",
+          body: JSON.stringify({ email, password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data.access_token);
 
-        vscode.postMessage({
-          command: "storeToken",
-          value: data.accessToken,
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    });
-})();
+      vscode.postMessage({
+        command: "setToken",
+        value: data.access_token,
+      });
+    } catch (error) {
+      vscode.postMessage({
+        command: "onError",
+        value: error,
+      });
+    }
+  });
--- media/main.js
+++ media/main.js
@@ -1,9 +1,31 @@
 (function () {
+  console.log(
+    "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  );
   const vscode = acquireVsCodeApi();
+
+  let token;
+
+  window.addEventListener("message", (event) => {
+    const message = event.data;
+    switch (message.command) {
+      case "setToken":
+        token = message.token;
+
+        break;
+    }
+  });
+
+  const button = document.getElementById("log");
+  button.addEventListener("click", () => {
+    console.log("somethong");
+  });
+  button.innerHTML = "text from js";
 
   const exitButton = document.getElementById("exit");
   const handleExit = () => {
     vscode.postMessage({ comand: "exit" });
+    console.log("exit");
   };
 
   exitButton.addEventListener("click", handleExit);
